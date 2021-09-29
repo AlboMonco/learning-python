8f9370af5f581e16e097d607d598507fd2c1198f
Enzo Albo Monaco <albomonaco@gmail.com>
2021-09-29 00:09:44 -0300

    3º Commit

---
+++ Learning how to work with strings.py
@@ -0,0 +1,15 @@
+#Strings are basically plain text.
+#Put the text in the next line : \n
+print("Nice Cake")
+print("Nice\nCake")
+print("Nice\"Cake")
+
+phrase = "Nice Cake"
+print(phrase + " is tasty")
+
+#phrase.lower()
+#phrase.upper()
+#Boolean: phrase.isupper() or phrase.islower()
+#Grab letters in a string: phrase[n], n a number of the position, starting with 0.
+#phrase.index("the letter/word/part_of_a_word ") gives me the position of the letter/word/part_of_the_word that I want.
+#phrase.replace("the thing that I want to delete", "the thing that I want to replace")
