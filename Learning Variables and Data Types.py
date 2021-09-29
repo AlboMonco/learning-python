3a698b9eff21385338b534ed8fa0d0e1b435f88f
Enzo Albo Monaco <albomonaco@gmail.com>
2021-09-28 23:28:30 -0300

    u7u

---
+++ Learning Variables and Data Types.py
@@ -0,0 +1,19 @@
+print("There once was a man named George, ")
+print("he was 70 years old.")
+print("He really liked the name George")
+print("but didn't like being 70")
+
+#We can change some parts of this text, and put variables. It's faster to put the variables when you want to change something in the code than change manually.
+#Let's create some variables.
+
+character_name = "John"
+character_age = "35"
+is_Male = True
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age +" years old.")
+print("He really liked the name " + character_name)
+print("but didn't like being "+ character_age)
+
+#This is a useful and an extremely important way to manage your data.
+#There are three basics types of data: Number(a number without quotation marks), String(text with quotation marks) and Boolean(True or False).
---
+++ Learning how to draw a shape.py
@@ -0,0 +1,5 @@
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+#The order of the instructions matters a lot.
