044e391f1611c2ac48d78d61395a040931b76019
Enzo Albo Monaco <albomonaco@gmail.com>
2021-10-01 16:07:46 -0300

    main

---
+++ Subjects/functions.py
@@ -0,0 +1,10 @@
+#Functions are collections of code which have a specific task.
+def say_hi():
+    print("Hi!")
+
+#Functions need the "calling". They have to be called to start to work.
+#Some functions can include parameters. I can put how many parameters I want. Eg: def say_hi(name): print("Hi " +name). Everytime that I want to run this functions, I will have to give a parameter, in this case, a name (str).
+#
+
+if __name__ == "__main__":
+    say_hi()
---
+++ Subjects/return_statement.py
@@ -0,0 +1,10 @@
+#Return statement:
+def cube(num):
+    return num**3
+    print("code") #it does not shows, because the program stop in "return" statement.
+
+result = cube(3)
+
+
+if __name__ == "__main__":
+    print(result)
