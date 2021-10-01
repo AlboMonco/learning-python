0bc2eb7977a64421456749decdb1ae51d3295113
Enzo Albo Monaco <albomonaco@gmail.com>
2021-09-30 23:30:00 -0300

    d

---
+++ Getting input from users.py
@@ -0,0 +1,7 @@
+#Input things
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + ".\nYou are " + age + " years old.")
+
+#It is used to get information from an user to put in the program.
