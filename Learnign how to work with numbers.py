67c13e778106cec9e3a00d7b70e17b7f5c9df6ed
Enzo Albo Monaco <albomonaco@gmail.com>
2021-09-30 22:31:39 -0300

    Numbers

---
+++ Learnign how to work with numbers.py
@@ -0,0 +1,13 @@
+#38:24 time of the video
+#it obeys all the math rules: ** = exponential, * = multiplication, / = division, // = exact division , % = it gives the remainer of the division, abs() = gives the absolute value of a number.
+#pow(num1, num2) = gives me the first number in the power of the second number, min(num1, num2) = gives me the smaller number, max(num1, num2) = gives me the higher number.
+#round(num1) = it rounds the num1.
+#importing: from math import *, or import math or from math import something
+
+from math import *
+
+my_number = 2
+print(my_number)
+
+#if I want to change the type of something, I can use the functions: int(), float(), str(), etc.
+#if I want to use an int with a string, I must change the type of the number to a string.
