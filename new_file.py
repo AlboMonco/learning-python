51269ad306cdccb9fe77f0a7813375ec09907efb
Enzo Albo Monaco <albomonaco@gmail.com>
2021-09-30 23:52:10 -0300

    First Calculator

--- new_file.py
+++ new_file.py
@@ -1,19 +1,25 @@
 from math import pow
 def calculator():
-    num1 = input("Enter the first number: ")
-    num2 = input("Enter the second number: ")
-    operation = input("Which operation do you want to use?\nPress 1 for multiplication\n2 for addition\n3 for division\n4 for exponential: ")
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    operation = int(input("Which operation do you want to use?\nPress 1 for multiplication\n2 for addition\n3 for division\n4 for exponential: "))
     if operation == 1:
         return num1*num2
     elif operation == 2:
         return num1 + num2
     elif operation == 3:
-        return num1/num2
+        if num2 == 0:
+            return "Can't determine"
+        else:
+            return num1/num2
     elif operation == 4:
-        return pow(num1, num2)
+        if num1 == 0 and num2 == 0:
+            return "Can't determine"
+        else: 
+            return pow(num1, num2)
     else:
-        return "This is not one of the choices"
-    
+        return "This is not a number of an operation. Please try again."
+
+
 if __name__ == "__main__":
-    calculator()
-    pass
+    print(calculator())
